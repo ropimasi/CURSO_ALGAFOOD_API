720ce4bf72ba3c79e03fb25f1a9311ddf7a2fae7
ropimasi <ropimasi@email.com>
2025-09-15 16:15:44 -0300

    Módulo 06 aula 01 concluída.

--- src/main/java/dev/ropimasi/curso/algafood/domain/model/Cozinha.java
+++ src/main/java/dev/ropimasi/curso/algafood/domain/model/Cozinha.java
@@ -1,18 +1,22 @@
 package dev.ropimasi.curso.algafood.domain.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonRootName;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 
 
 
-@JsonRootName(value = "culinaria")
+//@JsonRootName(value = "culinaria")
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
@@ -27,5 +31,9 @@ public class Cozinha {
 	//	@JsonProperty("titulo")
 	@Column(nullable = false)
 	private String nome;
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "cozinha")
+	private List<Restaurante> restaurantes = new ArrayList<>();
 
 }
--- src/main/java/dev/ropimasi/curso/algafood/domain/model/Restaurante.java
+++ src/main/java/dev/ropimasi/curso/algafood/domain/model/Restaurante.java
@@ -1,6 +1,7 @@
 package dev.ropimasi.curso.algafood.domain.model;
 
 import java.math.BigDecimal;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -30,6 +31,7 @@ public class Restaurante {
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
 
+//	@JsonIgnore
 	@ManyToOne
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
